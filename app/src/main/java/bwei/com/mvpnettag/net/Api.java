98531ba926d750432f7a3dafe2fbc2d1dac2ef48
Heyongwu <13366645979@163.com>
2017-11-22 10:23:12 +0800

    Initial commit

---
+++ app/src/main/java/bwei/com/mvpnettag/net/Api.java
@@ -0,0 +1,10 @@
+package bwei.com.mvpnettag.net;
+
+/**
+ * Created by peng on 2017/11/7.
+ */
+
+public interface Api {
+    public static final String ORDER = "https://www.zhaoapi.cn/product/getOrders";
+    public static final String CANCLE_ORDER = "https://www.zhaoapi.cn/product/updateOrder";
+}
---
+++ app/src/main/java/bwei/com/mvpnettag/net/OnNetListener.java
@@ -0,0 +1,14 @@
+package bwei.com.mvpnettag.net;
+
+/**
+ * Created by peng on 2017/11/7.
+ * 网络请求回调接口
+ */
+
+public interface OnNetListener<T> {
+    //成功回调
+    public void onSuccess(T t);
+
+    //失败回调
+    public void onFailure(Exception e);
+}
